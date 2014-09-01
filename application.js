0ebc26e6d0a68349b196395bb9cff2cd038145f2
Corinne Brady <corinnebrady@gmail.com>
2014-09-01 17:41:25 +1000

    rm

--- application.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('bookshelfApp', []);
-
-app.controller('BookshelfController', ['$scope', function($scope) {
-
-  $scope.books = ['1984', 'Wind-up Bird Chronicles', 'Dress Your Family in Curdoroy and Denim'];
-
-}]);
-
